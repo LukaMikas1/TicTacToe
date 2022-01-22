99d7ecc52540fe867d62216751c3c6dd89ae408f
LukaMikas1 <mikas.luka@gmail.com>
2022-01-23 00:16:01 +0100

    IsWin function
    Win state

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
 let boxes = document.querySelectorAll(".sqr")
 let value = 0
-// boxes.addEventListener("click", function(){
-//     console.log("DASDO")
-// })
+let game = true
+let boxList = []
+let getBox = function(boxes){
+    for(let i=0; i<boxes.length; i++){
+        return box = boxes[i]
+    }
+}
 
-// boxes.forEach(function (box){
-//     var boxes2 = box.querySelector(".sqr")
-//     if (boxes2){
-//         boxes2.addEventListener("click", function(){
-//         // boxes.forEach(function(item){
-//             console.log("oaksdo")
-//             // if (item !== box)
-//         })
-//     }
-// })
-
-for(let i=0; i<boxes.length; i++){
-    let box = boxes[i] 
-        box.addEventListener("click", function(){
-            if (box.firstChild.classList.contains("empty")){
-                if (value % 2 === 0){
-                    box.firstChild.classList.remove("empty")
-                    box.firstChild.classList.add("x")
+let isWin = function(){
+                if(boxes[0].firstChild.classList.contains("x") && boxes[1].firstChild.classList.contains("x") && boxes[2].firstChild.classList.contains("x") || boxes[3].firstChild.classList.contains("x") && boxes[4].firstChild.classList.contains("x") && boxes[5].firstChild.classList.contains("x") || boxes[6].firstChild.classList.contains("x") && boxes[7].firstChild.classList.contains("x") && boxes[8].firstChild.classList.contains("x") || boxes[0].firstChild.classList.contains("x") && boxes[3].firstChild.classList.contains("x") && boxes[6].firstChild.classList.contains("x") || boxes[1].firstChild.classList.contains("x") && boxes[4].firstChild.classList.contains("x") && boxes[7].firstChild.classList.contains("x") || boxes[2].firstChild.classList.contains("x") && boxes[5].firstChild.classList.contains("x") && boxes[8].firstChild.classList.contains("x") || boxes[0].firstChild.classList.contains("x") && boxes[4].firstChild.classList.contains("x") && boxes[8].firstChild.classList.contains("x") || boxes[2].firstChild.classList.contains("x") && boxes[4].firstChild.classList.contains("x") && boxes[6].firstChild.classList.contains("x")){
+                    console.log("X WINS!")
+                    return true
+                }
+                if(boxes[0].firstChild.classList.contains("o") && boxes[1].firstChild.classList.contains("o") && boxes[2].firstChild.classList.contains("o") || boxes[3].firstChild.classList.contains("o") && boxes[4].firstChild.classList.contains("o") && boxes[5].firstChild.classList.contains("o") || boxes[6].firstChild.classList.contains("o") && boxes[7].firstChild.classList.contains("o") && boxes[8].firstChild.classList.contains("o") || boxes[0].firstChild.classList.contains("o") && boxes[3].firstChild.classList.contains("o") && boxes[6].firstChild.classList.contains("o") || boxes[1].firstChild.classList.contains("o") && boxes[4].firstChild.classList.contains("o") && boxes[7].firstChild.classList.contains("o") || boxes[2].firstChild.classList.contains("o") && boxes[5].firstChild.classList.contains("o") && boxes[8].firstChild.classList.contains("o") || boxes[0].firstChild.classList.contains("o") && boxes[4].firstChild.classList.contains("o") && boxes[8].firstChild.classList.contains("o") || boxes[2].firstChild.classList.contains("o") && boxes[4].firstChild.classList.contains("o") && boxes[6].firstChild.classList.contains("o")){
+                    console.log("O WINS!")
+                    return true
                 }
                 else{
-                    box.firstChild.classList.remove("empty")
-                    box.firstChild.classList.add("o")
+                    return false
                 }
-            console.log(box.classList)
-            value++
-            }
-            else{
-               console.log(box.classList + "OKASD")
-               return
-                }
-        })
 }
+
+let runGame = function(){
+    if (!(isWin())){
+        for(let i=0; i<boxes.length; i++){
+            let box = boxes[i]
+            box.addEventListener("click", function(){
+                if (box.firstChild.classList.contains("empty")){
+                    if (value % 2 === 0){
+                        box.firstChild.classList.remove("empty")
+                        box.firstChild.classList.add("x")
+                    }
+                    else{
+                        box.firstChild.classList.remove("empty")
+                        box.firstChild.classList.add("o")
+                    }
+                value++
+                }
+                else{
+                return
+                    }
+                isWin()
+            })
+        }
+    }
+    else{
+        return
+    }
+}
+runGame()
